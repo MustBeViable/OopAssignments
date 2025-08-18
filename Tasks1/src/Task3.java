6153d305081271c00c1152ba317c25e9816a1326
MustBeViable <elias1rinne@gmail.com>
2025-08-18 12:34:44 +0300

    Added asked commentary

--- Tasks1/src/Task3.java
+++ Tasks1/src/Task3.java
@@ -2,31 +2,38 @@ import java.util.Scanner;
 
 public class Task3 {
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Give the first number:");
-            int first = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the first number:");
+        int first = Integer.parseInt(scanner.nextLine());
 
-            System.out.println("Give the second number:");
-            int second = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the second number:");
+        int second = Integer.parseInt(scanner.nextLine());
 
-            System.out.println("Give the third number:");
-            int third = Integer.parseInt(scanner.nextLine());
+        System.out.println("Give the third number:");
+        int third = Integer.parseInt(scanner.nextLine());
 
-            int sum = first + second + third;
-            int product = first * second * third;
+            /*After user declaration for numbers program calculates sum, product and average,
+              and rounds up correctly using double and wrong by using float
+             */
 
-            double average = ((first + second + third) / 3.0);
+        int sum = first + second + third;
+        int product = first * second * third;
 
-            //wrong float
-            float averageFloat = (float) ((first + second + third) / 3.0);
+        //Correctly rounds by using double
 
-            System.out.println("The sum of the numbers is " + (sum));
-            System.out.println("The product of the numbers is " + (product));
-            System.out.println("The average of the numbers using double is " + (average));
-            System.out.println("Somehow float rounds up too early by converting to bits and answer is different");
-            System.out.println("The average of the numbers using float is " + (averageFloat));
-        }
+        double average = ((first + second + third) / 3.0);
+
+        //Wrongly rounds up too much in the bit conversion phase.
+
+        float averageFloat = (float) ((first + second + third) / 3.0);
+
+        System.out.println("The sum of the numbers is " + (sum));
+        System.out.println("The product of the numbers is " + (product));
+        System.out.println("The average of the numbers using double is " + (average));
+        System.out.println("Somehow float rounds up too early by converting to bits and answer is different");
+        System.out.println("The average of the numbers using float is " + (averageFloat));
+    }
 
 }
