297dffeaec46cec2eaf1afb17f14f012fb50ecf5
MustBeViable <elias1rinne@gmail.com>
2025-08-18 13:28:32 +0300

    Completed Task2 from 1.2

---
+++ Tasks2/src/Task2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        //Creating scanner instance
+        Scanner scanner = new Scanner(System.in);
+
+        //Asking user values for the triangle
+        System.out.println("Give the length of the triangle: ");
+        double length = Double.parseDouble(scanner.nextLine());
+        System.out.println("Give the height of the triangle: ");
+        double height = Double.parseDouble(scanner.nextLine());
+        //Calculating area of the given triangle
+        double area = (length * height) / 2.0;
+
+        System.out.println("Given triangle area is: " + area);
+    }
+}
