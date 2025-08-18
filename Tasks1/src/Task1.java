76f611aa4d3430761895cb79bff3365a13b19677
MustBeViable <elias1rinne@gmail.com>
2025-08-18 10:49:11 +0300

    Task 1 compeleted

--- Tasks1/src/Task1.java
+++ Tasks1/src/Task1.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello teacher, me the best programmer in the world :--DD\n Yours truly,\n Elias Rinne");
+    }
 }
