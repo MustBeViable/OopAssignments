3460ead56005c6a3333675742964fbd5b2bfbc8d
MustBeViable <elias1rinne@gmail.com>
2025-08-18 12:04:40 +0300

    Task 1 fixed to correct refactor

--- Tasks1/src/Task2.java
+++ Tasks1/src/Task2.java
@@ -1,8 +1,15 @@
 public class Task2 {
     public static void main(String[] args) {
+        /*
         System.out.println("   *");
         System.out.println("  ***");
         System.out.println(" *****");
         System.out.println("*******");
+         */
+        System.out.println("    *\n" +
+                "   ***\n" +
+                "  *****\n" +
+                " *******\n" +
+                " ");
     }
 }
