f723d21c299de87a24b4744e505a49712f0d8393
MustBeViable <elias1rinne@gmail.com>
2025-08-18 13:31:33 +0300

    Modified how the answer is displayed

--- Tasks2/src/Task1.java
+++ Tasks2/src/Task1.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Give temperature as Fahrenheit:");
+        System.out.print("Give temperature as Fahrenheit: ");
         int fahrenheitDegree = Integer.parseInt(scanner.nextLine());
 
         //convert Fahrenheit to Celsius
 
         double celsius =  (fahrenheitDegree - 32) / (9.0/5.0);
-        System.out.println("Degrees converted to Celsius: " + celsius);
+        System.out.println("Degrees converted to Celsius: " + celsius + " C");
     }
 }
