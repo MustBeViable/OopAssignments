b6187c6813c7a98397ad689119ca37ff94cb1c22
MustBeViable <elias1rinne@gmail.com>
2025-08-20 13:36:49 +0300

    Task 1 completed and tested

---
+++ Week2.2Task1/src/GroceryListManager.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class GroceryListManager {
+    private ArrayList<String> groceryList = new ArrayList<>();
+
+    public void addItem(String item) {
+        groceryList.add(item);
+    }
+
+    public void removeItem(String item) {
+        if (this.checkItem(item)) {
+            System.out.println("\nRemoving " + item + " from the list...");
+            this.groceryList.remove(item);
+        } else {
+            System.out.println("\nYou tried to remove " + item + ". List does not contain " + item);
+        }
+    }
+
+    public void displayList() {
+        if (!this.groceryList.isEmpty()) {
+            System.out.println("\nGrocery List:");
+            int index = 1;
+            for (String grocery : this.groceryList) {
+                System.out.println(index + ". " + grocery);
+                index++;
+            }
+        }  else {
+            System.out.println("Grocery List is empty.");
+        }
+    }
+
+    public boolean checkItem(String item) {
+        return groceryList.contains(item);
+    }
+
+    public static void main(String[] args) {
+        GroceryListManager listManager = new GroceryListManager();
+        String[] groceries = {"Milk", "Bread", "Eggs", "Apples", "Potholders"};
+
+        for (String grocery : groceries) {
+            listManager.addItem(grocery);
+        }
+
+        listManager.displayList();
+
+        listManager.removeItem("Milk");
+
+        listManager.removeItem("Candy");
+
+        listManager.displayList();
+
+
+    }
+}
