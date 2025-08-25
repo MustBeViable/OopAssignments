ecb1bfe34fa9772f3ffe7feb4cf7250e9f714ce7
Elias <elias1rinne@gmail.com>
2025-08-25 14:11:40 +0300

    Task 4 completed and tested in 2.3

---
+++ Week2.3Task4/src/Book.java
@@ -0,0 +1,59 @@
+public class Book {
+    private String bookTitle;
+    private String author;
+    private int publicationYear;
+    double rating;
+    String review;
+
+    public Book(String title, String author, int year){
+        this.bookTitle = title;
+        this.author = author;
+        this.publicationYear = year;
+    }
+    public String getBookTitle (){
+        return this.bookTitle;
+    }
+
+    public void setBookTitle (String newTitle){
+        if (!newTitle.isEmpty()) {
+            this.bookTitle = newTitle;
+        }
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    public void setAuthor(String newAuthor) {
+        if (!newAuthor.isEmpty()) {
+            this.author = newAuthor;
+        }
+    }
+
+    public int getPublicationYear() {
+        return this.publicationYear;
+    }
+
+    public void setPublicationYear(int newYear) {
+        if (newYear > 0 && newYear <= 2025) {
+            this.publicationYear = newYear;
+        }
+    }
+
+    public double getRating() {
+        return this.rating;
+    }
+
+    public void setRating (double rating) {
+        this.rating = rating;
+    }
+
+    public String getReview() {
+        return this.review;
+    }
+
+    public void addReview(String review) {
+        this.review = review;
+    }
+
+}
