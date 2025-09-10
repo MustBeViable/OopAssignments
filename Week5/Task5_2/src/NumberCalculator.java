edce7b6793236264968e94feb6fcf1ac2262a59b
Elias <elias1rinne@gmail.com>
2025-09-10 12:14:53 +0300

    update numbers list in NumberCalculator to be static

--- Week5/Task5_2/src/NumberCalculator.java
+++ Week5/Task5_2/src/NumberCalculator.java
@@ -1,21 +1,26 @@
 import java.util.ArrayList;
 
 public class NumberCalculator extends Thread{
-    private final ArrayList<Integer> numbers;
+    private static ArrayList<Integer> numbers;
     private final int START_INDEX;
     private final int END_INDEX;
     private long sum;
 
-    public NumberCalculator (ArrayList<Integer> numbers, int startIndex, int END_INDEX) {
-        this.numbers = numbers;
+    public NumberCalculator (int startIndex, int END_INDEX) {
         this.START_INDEX = startIndex;
         this.END_INDEX = END_INDEX;
+    }
+
+    public static void setNumbers(ArrayList<Integer> list) {
+        if (!list.isEmpty()) {
+            numbers = list;
+        }
     }
 
     @Override
     public void run() {
         for (int i = 0; (i + this.START_INDEX)< (this.END_INDEX); i++) {
-            this.sum += this.numbers.get((i+this.START_INDEX));
+            this.sum += numbers.get((i+this.START_INDEX));
         }
     }
 
